39c246e8f1738ac6a52484c1f676ee0398733aca
Robert Nimmo <2379875n@student.gla.ac.uk>
2020-02-19 00:38:01 +0000

    chapter 11

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category,Page
 from rango.forms import CategoryForm,PageForm,UserForm, UserProfileForm
 from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
@@ -132,67 +132,8 @@ def add_page(request,category_name_slug):
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
-def register(request):
-    registered = False
-
-    if request.method =='POST':
-        user_form=UserForm(request.POST)
-        profile_form=UserProfileForm(request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user =user
-
-            if 'picture' in request.FILES:
-                profile.pictures = request.FILES['picture']
-
-            profile.save()
-
-            registered=True
-        else:
-            print(user_form.errors,profile_form.errors)
-    else:
-        user_form=UserForm()
-        profile_form=UserProfileForm()
-    return render(request,'rango/register.html',context ={'user_form':user_form,
-                                                'profile_form':profile_form,'registered':registered})
-
-def user_login(request):
-    if request.method =='POST':
-        username= request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username,password=password)
-
-        if user:
-            # Is the account active? It could have been disabled.
-            if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage. 
-                login(request,user)
-                return redirect(reverse('rango:index'))
-            else:
-                #An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
-        else:
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied")
-    else:
-        return render(request,'rango/login.html')
-
 @login_required
 def restricted(request):
     return render(request,'rango/restricted.html')
 
     # return HttpResponse("Since you're logged in, you can see this text!")
-
-@login_required
-def user_logout(request):
-    logout(request)
-
-    return redirect(reverse('rango:index'))
